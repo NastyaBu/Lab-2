aad8a8e0fb5cd33926eca7aad2ba6bced6c3fe70
Буданова Анастасия Евгеньевна <ri58s02@lwp08.csc.local>
2016-03-25 15:32:34 +0600

    Formula discriminant

--- main.c
+++ main.c
@@ -3,5 +3,17 @@
 
 int main()
 {
-	return 8;
+	int a, b, c, d;
+	
+	printf("Input firt element a:\n");
+	scanf("%d", &a);
+	
+	printf("Input second element b:\n");
+	scanf("%d", &b);
+	
+	printf("Input third element c:\n");
+	scanf("%d", &c);
+	
+	d = sqrt(b*b-4*a*c);
+	
 }
