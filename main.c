79754d71fee2d7516c346ad440fc1c6c88a83d7e
Буданова Анастасия Евгеньевна <ri58s02@awp05.csc.local>
2016-04-05 08:27:49 +0600

    Ispravlenie formatirovaniya

--- main.c
+++ main.c
@@ -3,28 +3,34 @@
 
 int main()
 {
-	int a, b, c, d, k1, k2;
+	float a = 0.0, b = 0.0, c = 0.0;
+	float d = 0.0;
+	float k1 = 0.0;
+	float k2 = 0.0;
 	
 	printf("Input firt element a:\n");
-	scanf("%d", &a);
+	scanf("%f", &a);
 	
 	printf("Input second element b:\n");
-	scanf("%d", &b);
+	scanf("%f", &b);
 	
 	printf("Input third element c:\n");
-	scanf("%d", &c);
+	scanf("%f", &c);
 	
-	d = b*b-4*a*c;
+	d = b * b - 4 * a * c;
 	
-	if (d >=0)
+	if (d >= 0)
 	{
-		k1 = (-b + sqrt(d))/(2*a);
-		k2 = (-b - sqrt(d))/(2*a);
+		k1 = (-b + sqrt(d)) / (2 * a);
+		k2 = (-b - sqrt(d)) / (2 * a);
 		
-		printf("K1: %d\n", k1);
-		printf("K2: %d\n", k2);
+		printf("K1: %f\n", k1);
+		printf("K2: %f\n", k2);
 	}
-	else printf ("disr < 0");
-	
+	else
+	{
+		printf("disr < 0");
+	}
+
 	return 0;
 }
